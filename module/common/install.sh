fc6dc6235390e3eb4252c907ee24b28782018b57
zhaoyang <zhaoyangliu10@gmail.com>
2026-09-12 00:17:49 +0800

    feat: Check HAL type before installation

--- module/common/install.sh
+++ module/common/install.sh
@@ -1,10 +1,15 @@
-echo -n $LIBPATCH > $MODPATH/libpatch.txt
+if service list 2>/dev/null | grep -qi 'android.hardware.audio.effect.IFactory'; then
+  ui_print "! AIDL audio effect HAL detected"
+  abort "! Use the AIDL module (ViPER4Android-RE-AIDL) instead"
+fi
+
+echo -n $LIBPATCH >$MODPATH/libpatch.txt
 
 ui_print "    Copying lib files..."
 
 cp_ch -n $MODPATH/common/files/libv4a_re_$ABI32.so $MODPATH$LIBDIR/lib/soundfx/libv4a_re.so
 if [ "$IS64BIT" ]; then
-cp_ch -n $MODPATH/common/files/libv4a_re_$ABI.so $MODPATH$LIBDIR/lib64/soundfx/libv4a_re.so
+  cp_ch -n $MODPATH/common/files/libv4a_re_$ABI.so $MODPATH$LIBDIR/lib64/soundfx/libv4a_re.so
 fi
 
 ui_print "    Patching audio_effects config files"
@@ -13,17 +18,17 @@ for OFILE in ${CFGS}; do
   FILE="$MODPATH$(echo $OFILE | sed "s|^/vendor|/system/vendor|g")"
   cp_ch -n $OFILE $FILE
   case $FILE in
-    *.conf)
-        sed -i "/v4a_standard_re {/,/}/d" $FILE
-        sed -i "/v4a_re {/,/}/d" $FILE
-        sed -i "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" $FILE
-        sed -i "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" $FILE
-        ;;
-    *.xml)
-        sed -i "/v4a_standard_re/d" $FILE
-        sed -i "/v4a_re/d" $FILE
-        sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" $FILE
-        sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" $FILE
-        ;;
+  *.conf)
+    sed -i "/v4a_standard_re {/,/}/d" $FILE
+    sed -i "/v4a_re {/,/}/d" $FILE
+    sed -i "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" $FILE
+    sed -i "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" $FILE
+    ;;
+  *.xml)
+    sed -i "/v4a_standard_re/d" $FILE
+    sed -i "/v4a_re/d" $FILE
+    sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" $FILE
+    sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" $FILE
+    ;;
   esac
 done
--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,19 +1,19 @@
-LIBPATCH=`cat $MODPATH/libpatch.txt`
+LIBPATCH=$(cat $MODPATH/libpatch.txt)
 CFGS="$(find /odm /system /vendor -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml")"
 for FILE in ${CFGS}; do
   case $FILE in
-    *.conf)
-        sed -i "/v4a_standard_re {/,/}/d" $FILE
-        sed -i "/v4a_re {/,/}/d" $FILE
-        sed -i "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" $FILE
-        sed -i "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" $FILE
-        ;;
-    *.xml) 
-        sed -i "/v4a_standard_re/d" $FILE
-        sed -i "/v4a_re/d" $FILE
-        sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" $FILE
-        sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" $FILE
-        ;;
+  *.conf)
+    sed -i "/v4a_standard_re {/,/}/d" $FILE
+    sed -i "/v4a_re {/,/}/d" $FILE
+    sed -i "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" $FILE
+    sed -i "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" $FILE
+    ;;
+  *.xml)
+    sed -i "/v4a_standard_re/d" $FILE
+    sed -i "/v4a_re/d" $FILE
+    sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" $FILE
+    sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" $FILE
+    ;;
   esac
 done
 
